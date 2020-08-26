42c35ab4010c34be46c1f7087d64edf261dc0bd8
Debarghya Bandyopadhyay <debarghya.85@gmail.com>
2020-08-26 08:15:10 +0530

    basic implementation of promotion apply

--- src/main/java/org/codetest/promotionengine/service/PromotionCalculatorHelper.java
+++ src/main/java/org/codetest/promotionengine/service/PromotionCalculatorHelper.java
@@ -13,6 +13,74 @@ import java.util.stream.Collectors;
 
 @Component
 public class PromotionCalculatorHelper {
+    private Map<String,Double> itemSkuDefaultPrices = new HashMap<>();
+
+    public PromotionCalculatorHelper() {
+        itemSkuDefaultPrices.put("A",50.0);
+        itemSkuDefaultPrices.put("B",30.0);
+        itemSkuDefaultPrices.put("C",20.0);
+        itemSkuDefaultPrices.put("D",15.0);
+    }
+
+
+    public double calculateFinalPrice(List<Promotion> promotions,List<PromotionItemSKU> itemsOrdered,Map<String,Integer> promotionAppliedItems){
+        AtomicReference<Double> finalPrice = new AtomicReference<>((double) 0);
+        Map<String,Integer> itemPriceCalculated = new HashMap<String,Integer>();
+        Map<String, Integer> orderedItems = new HashMap<>();
+        itemsOrdered.stream().forEach(item->orderedItems.put(item.getSku(),item.getQuantity()));
+        final Map<String, Integer>[] promotionalItems = new Map[]{new HashMap<>()};
+        final Map<String, Integer>[] remainingItems = new Map[]{new HashMap<>()};
+        promotions.stream().forEach(promotion ->{
+            remainingItems[0] = new HashMap<>();
+            promotionalItems[0] = new HashMap<>();
+            if(isPromotionEligible(promotion,itemsOrdered,promotionAppliedItems)){
+
+                final int[] promotionApplicableTimes = {0};
+                promotion.getPromotionItemSKUs().stream().forEach(promoItem->{
+                    if(promotionApplicableTimes[0]==0) {
+                        promotionApplicableTimes[0] = orderedItems.get(promoItem.getSku()) / promoItem.getQuantity();
+                    }
+                    promotionalItems[0].put(promoItem.getSku(),(promoItem.getQuantity()* promotionApplicableTimes[0]));
+                    promotionAppliedItems.put(promoItem.getSku(),(promoItem.getQuantity()* promotionApplicableTimes[0]));
+                });
+
+                orderedItems.entrySet().stream().forEach(itemEntry->{
+                    if(promotionalItems[0].containsKey(itemEntry.getKey()) )
+                        remainingItems[0].put(itemEntry.getKey(),(itemEntry.getValue() - promotionalItems[0].get(itemEntry.getKey())));
+                });
+
+                finalPrice.updateAndGet(v -> new Double((double) (v + promotion.getPromotionalPrice()*promotionApplicableTimes[0])));
+                remainingItems[0].entrySet().stream().forEach(remainingEntry->{
+                    if(!itemPriceCalculated.containsKey(remainingEntry.getKey())) {
+                        finalPrice.updateAndGet(v -> new Double((double) (v + (remainingEntry.getValue() * itemSkuDefaultPrices.get(remainingEntry.getKey())))));
+                        itemPriceCalculated.put(remainingEntry.getKey(),remainingEntry.getValue());
+                    }
+                });
+            }
+        });
+
+        orderedItems.entrySet().stream().forEach(itemEntry->{
+            if(!promotionalItems[0].containsKey(itemEntry.getKey()) && !itemPriceCalculated.containsKey(itemEntry.getKey())) {
+                finalPrice.updateAndGet(v -> new Double((double) (v + (itemEntry.getValue() * itemSkuDefaultPrices.get(itemEntry.getKey())))));
+            }
+        });
+
+
+        return finalPrice.get();
+    }
+
+
+    private boolean isPromotionMutualExclusive(Promotion promotion, Map<String,Integer> promotionAppliedItems){
+        boolean isPossible = true;
+
+        return isPossible;
+    }
+
+    public boolean isPromotionEligible(Promotion promotion,List<PromotionItemSKU> itemOrdered,Map<String,Integer> promotionAppliedItems){
+        boolean isEligible = true;
+
+        return isEligible;
+    }
 
 
 }
--- src/main/java/org/codetest/promotionengine/service/PromotionServiceImpl.java
+++ src/main/java/org/codetest/promotionengine/service/PromotionServiceImpl.java
@@ -21,12 +21,19 @@ public class PromotionServiceImpl implements PromotionService {
 
 
     @Override
-    public List<Promotion> getAllPromotions() {
-        return null;
+    public List<Promotion> getAllPromotions(){
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        promotions.add(new Promotion("3 of A's for 130",130.00,new ArrayList<PromotionItemSKU>(Arrays.asList(new PromotionItemSKU("A",3)))));
+        promotions.add(new Promotion("2 of B's for 45",45.00,new ArrayList<PromotionItemSKU>(Arrays.asList(new PromotionItemSKU("B",2)))));
+        promotions.add(new Promotion("C and D for 30",30.00,new ArrayList<PromotionItemSKU>(Arrays.asList(new PromotionItemSKU("C",1),new PromotionItemSKU("D",1)))));
+
+        return promotions;
     }
 
     @Override
-    public double applyPromotion(List<PromotionItemSKU> promotionItemSKUs) {
-        return 0;
+    public double applyPromotion(List<PromotionItemSKU> itemOrdered) {
+        List<Promotion> promotionsList = getAllPromotions();
+        promotionAppliedItems = new HashMap<>();
+        return promotionCalcHelper.calculateFinalPrice(promotionsList,itemOrdered,promotionAppliedItems);
     }
 }
